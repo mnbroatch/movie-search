3ecdda9bf9eb280e9929aedbbfe38acf4f125d0b
Matthew Broatch <mnbroatch@gmail.com>
2016-06-14 12:30:50 -0700

    some changes

--- main.js
+++ main.js
@@ -4,29 +4,6 @@ const URL_START = "http://www.omdbapi.com/?s=";
 
 var PAGES_ARRAY = [];
 
-function Result(type, title, year, genre, plot, imdb, starring){
-	this.type = type;
-	this.title = title;
-	this.year = "(" + year + ")";
-	this.genre = genre;
-	this.plot = plot;
-	this.starring = starring;
-	this.imdb = "http://www.imdb.com/title/" + imdb;
-}
-
-Object.defineProperty(Result.prototype,'$element', {get: function(){					
-	var $resultElementToAdd = $('#result-template').clone();
-	$resultElementToAdd.removeAttr('id','result-template');
-	$resultElementToAdd.find('.title').text(this.title);
-	$resultElementToAdd.find('.title').attr('href',this.imdb);
-	$resultElementToAdd.find('.year').text(this.year);
-	$resultElementToAdd.find('.starring').text(this.starring);
-	$resultElementToAdd.find('.plot').text(this.plot);
-	$resultElementToAdd.find('.genre').text(this.genre);
-	return $resultElementToAdd;
-}});
-
-
 
 $(document).ready( function(){
 
@@ -43,9 +20,31 @@ $(document).ready( function(){
 		getPageOfResults(1,searchURL);
 
 	});
-
 });
 
+
+function Result(type, title, year, genre, plot, imdb, starring){
+	this.type = type;
+	this.title = title;
+	this.year = year;
+	this.genre = genre;
+	this.plot = plot;
+	this.starring = starring;
+	this.imdb = "http://www.imdb.com/title/" + imdb;
+}
+Object.defineProperty(Result.prototype,'$element', {get: get$element });
+
+function get$element (){					
+	var $resultElementToAdd = $('#result-template').clone();
+	$resultElementToAdd.removeAttr('id','result-template');
+	$resultElementToAdd.find('.title').text(this.title);
+	$resultElementToAdd.find('.title').attr('href',this.imdb);
+	$resultElementToAdd.find('.year').text("(" + this.year + ")");
+	$resultElementToAdd.find('.starring').text("Starring " + this.starring);
+	$resultElementToAdd.find('.plot').text(this.plot);
+	$resultElementToAdd.find('.genre').text(this.genre);
+	return $resultElementToAdd;
+}
 
 
 function formatSearch(titleToGet,typeToGet,yearToGet) {
@@ -64,11 +63,9 @@ function formatSearch(titleToGet,typeToGet,yearToGet) {
 
 
 function getPageOfResults(pageToGet,searchTerm) {
-	console.log(searchTerm + "&page=" + pageToGet);
 
 	$.ajax(searchTerm + "&page=" + pageToGet, {
 		success: function(data) {
-			console.log(data);
 
 			if( data.Response != "False"){
 				var $resultsArea = $('#results-area');
@@ -85,7 +82,7 @@ function getPageOfResults(pageToGet,searchTerm) {
 							var genre = data.Genre;
 							var plot = data.Plot;
 							var type = data.Type;
-							var starring = data.Starring;
+							var starring = data.Actors;
 							var resultToAdd = new Result(type, title, year, genre, plot, imdb, starring);
 
 							$resultsToAppend.push(resultToAdd.$element);
